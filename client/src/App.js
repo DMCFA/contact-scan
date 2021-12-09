88bfd08e6c9b7f414d1a0244fd00d94cbd7bf1c1
Duarte Almeida <duartemalmeida@gmail.com>
2021-12-09 20:17:11 +0000

    feat! add sass to project and re-do stylesheets and imports

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from 'react';
 import { getAllContacts } from './routes/api';
 import Home from './components/Home';
 import Loading from './components/Loading';
-import './App.css';
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
