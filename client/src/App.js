e2ed0046cddfb71aabbfd24f2b195034cabd5860
Duarte Almeida <duartemalmeida@gmail.com>
2021-12-09 17:44:25 +0000

    feat! add loading and contacts local state add a useEffect function to run at start to fetch all contacts and set them to the contacts while setting loading to false and conditionally displaying components accordingly

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,26 @@
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from 'react';
+import { getAllContacts } from './routes/api';
+import Home from './components/Home';
+import Loading from './components/Loading';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    getAllContacts().then((results) => {
+      setIsLoading(false);
+      setContacts(results);
+    });
+  }, []);
+
+  return (
+    <div className='container'>
+      <div>{isLoading ? <Loading /> : <Home contacts={contacts} />}</div>
+    </div>
+  );
 }
 
 export default App;
