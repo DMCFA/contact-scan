bf99bde32ab1175cc70b3e29455f6ec79f8aa37e
Duarte Almeida <duartemalmeida@gmail.com>
2021-12-11 19:51:45 +0000

    feat! add router, routes and route to function

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { getAllContacts } from './routes/api';
 import Home from './components/Home';
 import Loading from './components/Loading';
+import Contact from './components/Contact';
+import ContactForm from './components/ContactForm';
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -16,9 +19,22 @@ function App() {
   }, []);
 
   return (
-    <div className='container'>
-      <div>{isLoading ? <Loading /> : <Home contacts={contacts} />}</div>
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          exact
+          path='/'
+          element={
+            <div className='container'>
+              <div>
+                {isLoading ? <Loading /> : <Home contacts={contacts} />}
+              </div>
+            </div>
+          }
+        ></Route>
+        <Route path='/new' component={ContactForm}></Route>
+      </Routes>
+    </Router>
   );
 }
 
