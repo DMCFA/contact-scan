919d8473c86e4ebb5ed0a5254c4129538c3e3054
Duarte Almeida <duartemalmeida@gmail.com>
2021-12-18 12:09:00 +0000

    feat!: add edit and delete buttons and create onClick functions for desired behaviour

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,22 @@
-/* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { NavBar } from './NavBar';
+import { deleteContact } from '../routes/api';
 
 const Home = ({ contacts }) => {
+  const navigate = useNavigate();
   const [searchWord, setSearchWord] = useState('');
+
+  const handleEdit = (e, id) => {
+    e.preventDefault();
+    navigate(`/edit/${id}`);
+  };
+
+  const handleDelete = (e, id) => {
+    e.preventDefault();
+    deleteContact(id);
+    navigate('/');
+  };
 
   const filteredNames = contacts.filter((person) =>
     person.name.toLowerCase().includes(searchWord.toLowerCase())
@@ -11,7 +24,19 @@ const Home = ({ contacts }) => {
 
   const contactName = filteredNames.map((contact) => (
     <li className='contacts-line' key={contact.id}>
-      <a href={'/' + contact.id}>{contact.name}</a>
+      <a href={'/' + contact.id}>
+        {contact.name}
+        <span className='icons'>
+          <i
+            className='icon far fa-edit'
+            onClick={(e) => handleEdit(e, contact.id)}
+          ></i>
+          <i
+            className='icon far fa-trash-alt'
+            onClick={(e) => handleDelete(e, contact.id)}
+          ></i>
+        </span>
+      </a>
     </li>
   ));
 
