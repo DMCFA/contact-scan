0a7ac9d29871aad067432629b961c893da47ea69
Duarte Almeida <duartemalmeida@gmail.com>
2021-12-20 22:47:24 +0000

    fix: useeffect was only triggering on first render and not when redirected from a form submission

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, ma } from 'react-router-dom';
 import { getAllContacts } from './routes/api';
 import Home from './components/Home';
 import Loading from './components/Loading';
@@ -9,28 +9,34 @@ import ContactForm from './components/ContactForm';
 import EditForm from './components/EditForm';
 
 function App() {
+  const [dataChanged, setDataChanged] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const [contacts, setContacts] = useState([]);
 
   useEffect(() => {
     getAllContacts().then((results) => {
+      setDataChanged(false);
       setIsLoading(false);
       setContacts(results);
     });
-  }, []);
+  }, [dataChanged]);
 
   return (
     <div className='container'>
       <Routes>
         <Route
           path='/'
-          element={
-            <div>{isLoading ? <Loading /> : <Home contacts={contacts} />}</div>
-          }
+          element={isLoading ? <Loading /> : <Home contacts={contacts} />}
         ></Route>
-        <Route path='/new' element={<ContactForm />}></Route>
+        <Route
+          path='/new'
+          element={<ContactForm changes={setDataChanged} />}
+        ></Route>
         <Route path='/:id' element={<Contact />}></Route>
-        <Route path='/edit/:id' element={<EditForm />}></Route>
+        <Route
+          path='/edit/:id'
+          element={<EditForm changes={setDataChanged} />}
+        ></Route>
       </Routes>
     </div>
   );
