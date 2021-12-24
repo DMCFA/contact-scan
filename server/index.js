bcba241ff939b5d1a74c98e8ecb043e0c6619fbb
Duarte Almeida <duartemalmeida@gmail.com>
2021-12-24 15:06:22 +0000

    add frontend build location

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import router from './routes/contacts.js';
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, './../client/build')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
