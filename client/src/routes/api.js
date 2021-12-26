615a49cd16888522e335e5e497e8cc3718221fa1
Duarte Almeida <duartemalmeida@gmail.com>
2021-12-26 18:08:11 +0000

    fix: change base url on client routes to online server address

--- client/src/routes/api.js
+++ client/src/routes/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3001/api';
+const baseUrl = 'https://contact-scan.herokuapp.com/api';
 
 // @route    GET "/"
 // @desc.    Get all contacts
